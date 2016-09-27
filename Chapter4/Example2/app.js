7078e3aa8ad1104064bd6b7cc71553623364fb8a
Jinad Patel <jinadpatel@csu.fullerton.edu>
2016-09-27 13:15:51 -0700

    Chapter 4 app.js and style.css modified

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,42 +1,38 @@
 /* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
-var main = function () {
+var main = function() {
     'use strict';
-
-    
-    var addCommentsInputBox2 = function (user,add) {
+    var addCommentsInputBox2 = function(user, add) {
         var $newComment;
-
         if (user !== ' ') {
-            $newComment = $('<p class='+add+'>').text(user);
+            $newComment = $('<p class=' + add + '>').text(user);
             $newComment.hide();
             $('.comments').append($newComment);
-            $newComment.fadeIn();   
+            $newComment.fadeIn();
         }
     };
-
-    $('.comment-inputuser1 button').on('click', function () {
-        addCommentsInputBox2($('.comment-inputuser1 input').val(),'user1');
-        $('.comment-inputuser1 input').val('');   
+    $('.comment-inputuser1 button').on('click', function() {
+        addCommentsInputBox2($('.comment-inputuser1 input').val(),
+            'user1');
+        $('.comment-inputuser1 input').val('');
     });
-
-    $('.comment-inputuser1 input').on('keypress', function (event) {
+    $('.comment-inputuser1 input').on('keypress', function(event) {
         if (event.keyCode === 13) {
-            addCommentsInputBox2($('.comment-inputuser1 input').val(),'user1');
-            $('.comment-inputuser1 input').val('');        
+            addCommentsInputBox2($('.comment-inputuser1 input').val(),
+                'user1');
+            $('.comment-inputuser1 input').val('');
         }
     });
-
-    $('.comment-inputuser2 button').on('click', function () {
-        addCommentsInputBox2($('.comment-inputuser2 input').val(),'user2');
-        $('.comment-inputuser2 input').val('');   
+    $('.comment-inputuser2 button').on('click', function() {
+        addCommentsInputBox2($('.comment-inputuser2 input').val(),
+            'user2');
+        $('.comment-inputuser2 input').val('');
     });
-
-    $('.comment-inputuser2 input').on('keypress', function (event) {
+    $('.comment-inputuser2 input').on('keypress', function(event) {
         if (event.keyCode === 13) {
-            addCommentsInputBox2($('.comment-inputuser2 input').val(),'user2');
-            $('.comment-inputuser2 input').val('');   
+            addCommentsInputBox2($('.comment-inputuser2 input').val(),
+                'user2');
+            $('.comment-inputuser2 input').val('');
         }
     });
 };
-
 $(document).ready(main);
